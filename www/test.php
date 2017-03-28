b2a3c4f98cd4fea3c41f526e1d5ac6817831dbbc
segun <gkokoete@yahoo.com>
2017-03-28 10:52:57 +0100

    finish config for text.php

--- www/test.php
+++ www/test.php
@@ -5,5 +5,5 @@
 define('DBNAME', 'virtualmall');
 define('DBUSER', 'root');
 define('DBPASS', 'zobo');
-$pdo = new PDO('mysql:host=localhost;dbname=virtualmall', "root", "zobo");
+$pdo = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
 ?>
