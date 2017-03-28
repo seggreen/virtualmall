db8f9a26e84dec7f4a9c791a9c88b8657397b8b7
segun <gkokoete@yahoo.com>
2017-03-28 10:34:26 +0100

    finish config for index.php

--- www/index.php
+++ www/index.php
@@ -1,11 +1,4 @@
 <?php 
-#db connection file
 
-#define db constants
-//define('DBNAME', 'bookstore');
-//define('DBUSER', 'root');
-//define('DBPASS', 'zobo');
-
-//$pdo = new PDO('mysql:host=localhost;dbname=bookstore', "root", "zobo");
 ?>
 
