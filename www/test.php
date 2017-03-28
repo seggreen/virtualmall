281ad141088eff016731b7e73629acd27ed2ae28
segun <gkokoete@yahoo.com>
2017-03-28 11:15:02 +0100

    re-editing text.php syntax

--- www/test.php
+++ www/test.php
@@ -5,5 +5,15 @@
 define('DBNAME', 'virtualmall');
 define('DBUSER', 'root');
 define('DBPASS', 'zobo');
+
+try{
+	#prepare pdo instance
 $pdo = new PDO('mysql:host=localhost;dbname='.DBNAME, DBUSER, DBPASS);
+
+#set verbose error modes
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+
+}catch(PDOException $e) {
+	echo $e->getMessage();
+}
 ?>
