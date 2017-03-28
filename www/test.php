c13ef37e27b1fe925fc4b181af9a483149824c11
segun <gkokoete@yahoo.com>
2017-03-28 10:50:08 +0100

    finish editing text.php

--- www/test.php
+++ www/test.php
@@ -1,4 +1,9 @@
 <?php 
+#db connection file
 
+#define db constants
+define('DBNAME', 'virtualmall');
+define('DBUSER', 'root');
+define('DBPASS', 'zobo');
 $pdo = new PDO('mysql:host=localhost;dbname=virtualmall', "root", "zobo");
 ?>
