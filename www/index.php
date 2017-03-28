57773aa52aa1eae0d5cc2aca421fe519d4078133
segun <gkokoete@yahoo.com>
2017-03-28 10:20:00 +0100

    finish modify index.php and test.php

--- www/index.php
+++ www/index.php
@@ -1,1 +1,11 @@
-<?php echo "ok oooh"; ?>
+<?php 
+#db connection file
+
+#define db constants
+//define('DBNAME', 'bookstore');
+//define('DBUSER', 'root');
+//define('DBPASS', 'zobo');
+
+$pdo = new PDO('mysql:host=localhost;dbname=virtualmall', "root", "zobo");
+?>
+
