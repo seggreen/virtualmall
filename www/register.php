7a96ddb55b24b40a153fea5ff1125c296bf8646a
segun <gkokoete@yahoo.com>
2017-03-29 17:00:53 +0100

    Finish error validation

--- www/register.php
+++ www/register.php
@@ -8,46 +8,67 @@ $page_title = "Register";
  	#cache errors....
  	$errors = [];
 
- 	#VALIDATION.....
+ 	#VALIDATE FIRST NAME.....
  	if(empty($_POST['fname'])) {
- 		$errors[] = "Please enter first name";
+ 		$errors['fname'] = "Please enter first name";
+ 	}
+ 	
+ 	#VALIDATE LAST NAME.....
+ 	if(empty($_POST['lname'])) {
+ 		$errors['lname'] = "Please enter last name";
+ 	}
+ 	
+ 	#VALIDATE EMAIL.....
+ 	if(empty($_POST['email'])) {
+ 		$errors['email'] = "Please enter email";
  	}
 
+ 	#VALIDATE PASSWORD.....
+ 	if(empty($_POST['password'])) {
+ 		$errors['password'] = "Please enter password";
+ 	}
+
+ 	#VALIDATE SECOND PASSWORD.....
+ 	if($_POST['password'] != $_POST['pword']) {
+ 		$errors['pword'] = "password do not match";
+ 	}	
+ 		
  	if(empty($errors)) {
  		//do DB stuff.....
- 	} else {
- 		foreach ($errors as $err) {
- 			# code...
- 			echo $err;
- 		}
- 	}
+ 	} 
  }
 
 ?>
+
 
 <div class="wrapper">
 		<h1 id="register-label">Admin Register</h1>
 		<hr>
 		<form id="register"  action ="register.php" method ="POST">
 			<div>
+			    <?php if(isset($errors['fname'])) { echo '<span class="err">'. $errors['fname']. '</span>';} ?>
 				<label>first name:</label>
 				<input type="text" name="fname" placeholder="first name">
 			</div>
 			<div>
+			    <?php if(isset($errors['lname'])) { echo '<span class="err">'. $errors['lname']. '</span>';} ?>
 				<label>last name:</label>	
 				<input type="text" name="lname" placeholder="last name">
 			</div>
 
 			<div>
+			    <?php if(isset($errors['email'])) { echo '<span class="err">'. $errors['email']. '</span>';} ?>
 				<label>email:</label>
 				<input type="text" name="email" placeholder="email">
 			</div>
 			<div>
+			    <?php if(isset($errors['password'])) { echo '<span class="err">'. $errors['password']. '</span>';} ?>
 				<label>password:</label>
 				<input type="password" name="password" placeholder="password">
 			</div>
  
 			<div>
+			    <?php if(isset($errors['pword'])) { echo '<span class="err">'. $errors['pword']. '</span>';} ?>
 				<label>confirm password:</label>	
 				<input type="password" name="pword" placeholder="password">
 			</div>
@@ -57,6 +78,7 @@ $page_title = "Register";
 
 		<h4 class="jumpto">Have an account? <a href="login.php">login</a></h4>
 	</div>
+
 <?php
 
    #include footer.....
