94a52d4636646d0e37efffd3f31993a4d0217ce1
segun <gkokoete@yahoo.com>
2017-03-31 08:44:00 +0100

    finish config for functions.php 2

--- www/includes/functions.php
+++ www/includes/functions.php
@@ -1,10 +1,10 @@
 <?php
-    function doAdminRegister($dbconn, $input) {
+    function doAdminRegister($conn, $input) {
     	#HASH PASSWORD.....
     	$hash = password_hash($input['password'], PASSWORD_BCRYPT);
 
     	#INSERT DATA.....
-    	$stmt = $dbconn->prepare("INSERT INTO adnim(firstname, lastname, email, hash) VALUES(:fn, :ln, :e, :h)");
+    	$stmt = $conn->prepare("INSERT INTO adnim(firstname, lastname, email, hash) VALUES(:fn, :ln, :e, :h)");
 
     	#BIND PARAMS.....
     	$data =[':fn'=>$input['fname'], ':ln'=>$input['lname'], ':$input'=>$input['email'], ':h'=>$hash];
